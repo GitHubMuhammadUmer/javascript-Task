eed50b06e5213a84785566ca23b1738e681514d7
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2023-12-09 02:26:02 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+
+//     --------------Task_1------------------ 
+
+// var userName = prompt("Enter User Name");
+// var phoneNumber = +prompt("Enter Phone Number");
+// var email = prompt("Enter Email");
+// var password = prompt("Enter Password");
+// var confirmPassword = prompt("Confirm Password");
+// if(!userName || !phoneNumber || !email  || !password || !confirmPassword ){
+//     document.write("Error Please Fill ")
+
+// }
+// else if(password===confirmPassword){
+//     document.write("Succesfully Registered")
+//     }
+   
+// else{
+//     document.write("Password Do Not Correct")
+
+// }
+
+
+
+
+
+//     --------------Task_2------------------ 
+
+// palindrom
+
+// var inputUser = prompt("Enter any word")
+// var data='';
+// for(i=inputUser.length-1; i>=0; i--){
+//     data+=inputUser[i]
+//     // console.log(i,inputUser[i])
+// }
+// if(data===inputUser){
+//     console.log(inputUser+"is palindrom")
+// }else{
+//     console.log(inputUser+" "+"is not palindrom")
+// }
+
+
+
+
+
+//
+//     --------------Task_3------------------ 
+// var numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// var evan = [];
+// var odd = [];
+// for(var i=0; i < numberArray.length ; i++){
+
+// if(numberArray[i] % 2 === 0){
+//     evan.push(numberArray[i])
+// }else{
+//     odd.push(numberArray[i])
+// }
+// }
+// document.write("Evan Number" + " " + evan + "</br>") 
+// document.write("Odd Number" + " " + odd)
